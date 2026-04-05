12293f92e71feb1f61e88f22159b93f57b74a884
denis <den.podgorod@gmail.com>
2026-04-05 18:59:53 +0300

    HomeWork_6 FirstTest

---
+++ Dz1/HomeWork_6/FirstTest.py
@@ -0,0 +1,48 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+options = webdriver.ChromeOptions()
+# Сделалил кастомное правило которе не закрывает браузер автоматически после завершения скрипта
+options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
+
+try:
+    base_url = 'https://www.saucedemo.com/'
+    driver.get(base_url)
+    driver.maximize_window()
+
+# Находим на странице поле для ввода имени
+    user_name = driver.find_element(By.ID, 'user-name')
+    user_name.send_keys('standard_user') # Ввод имени в поле "usesname"
+    print("Username passed")
+# С помощью другого типа XPATH (*) означает что тут мы ищем по всей странице Id = 'password'
+    password = driver.find_element(By.ID, 'password')
+    password.send_keys('secret_sauce') # Ввод пароля в поле "password"
+    print("Password passed")
+#Добавим переменную для поиска нужной кнопки на странице
+    button_login = driver.find_element(By.ID, 'login-button')
+    button_login.click()
+    print("Login button pressed")
+
+    current_url = driver.current_url
+    print(driver.current_url)
+
+    assert current_url == 'https://www.saucedemo.com/inventory.html'
+    print("Url cовпало")
+
+    page_check = driver.find_element(By.XPATH, "//span[@class='title']")
+    print(page_check.text)
+    page_check = page_check.text
+    assert page_check == 'Product'
+    print("Находимся на нужной странице")
+
+except Exception:
+    # Этот блок сработает только при ошибке
+    print("Тест упал с ошибкой")
+
+finally:
+    driver.quit()
