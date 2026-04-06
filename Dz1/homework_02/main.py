edf87dcf67bafa29e18118d476058b7d825986ce
denis <den.podgorod@gmail.com>
2026-04-06 11:36:34 +0300

    Style: fix directory structure and naming conventions

---
+++ Dz1/homework_02/main.py
@@ -0,0 +1,38 @@
+import sys
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.edge.service import Service as EdgeService
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+
+
+def start_test(browser_name: str) -> None:
+
+    driver: webdriver.Remote
+
+    if browser_name == 'chrome':
+        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    elif browser_name == 'firefox':
+        driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+    elif browser_name == 'edge':
+        driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()))
+    else:
+        print(f"Ошибка: браузер {browser_name} не найден. Используйте: chrome, firefox или edge")
+        return
+
+    # Логика теста
+    driver.get('https://saucedemo.com')
+    driver.maximize_window()
+    time.sleep(3)
+    driver.quit()
+
+if __name__ == "__main__":
+    # Проверяем, передал ли пользователь название браузера в терминале
+    if len(sys.argv) > 1:
+        chosen_browser = sys.argv[1].lower()
+        start_test(chosen_browser)
+    else:
+        print("Пожалуйста, укажите браузер. Пример: python main.py chrome")
---
+++ Dz1/homework_05/Login.py
@@ -0,0 +1,31 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+options = webdriver.ChromeOptions()
+# Сделалил кастомное правило которе не закрывает браузер автоматически после завершения скрипта
+options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
+
+base_url = 'https://www.saucedemo.com/'
+driver.get(base_url)
+driver.maximize_window()
+
+# Находим на странице поле для ввода имени
+user_name = driver.find_element(By.ID, 'user-name')
+user_name.send_keys('standard_user') # Ввод имени в поле "usesname"
+time.sleep(1) # указываем таймер паузы для проверки корректного ввода
+
+# С помощью другого типа XPATH (*) означает что тут мы ищем по всей странице Id = 'password'
+password = driver.find_element(By.ID, 'password')
+password.send_keys('secret_sauce') # Ввод пароля в поле "password"
+
+#Добавим переменную для поиска нужной кнопки на странице
+button_login = driver.find_element(By.ID, 'login-button')
+button_login.click()
+
+time.sleep(1) # указываем таймер паузы для проверки корректного ввода
+driver.quit() # Команда для полного закрытия окна браузера
