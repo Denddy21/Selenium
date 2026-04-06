e3a474c0fe196247f8b602ef9dbb4f0e51f2d4f5
denis <den.podgorod@gmail.com>
2026-04-06 12:01:31 +0300

    style: fix logical blocks and add type annotations for xpath homework

--- Dz1/homework_04/hw_04_xpath_practice.py
+++ Dz1/homework_04/hw_04_xpath_practice.py
@@ -5,24 +5,27 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 
 
-options = webdriver.ChromeOptions()
-# Сделалил кастомное правило которе не закрывает браузер автоматически после завершения скрипта
+# Настройка опций браузера
+options: webdriver.ChromeOptions = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
 
-driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
+# Инициализация драйвера
+driver: webdriver.Chrome = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
 
-base_url = 'https://www.saucedemo.com/'
+# Открытие сайта
+base_url: str = 'https://www.saucedemo.com/'
 driver.get(base_url)
 driver.maximize_window()
 
-# Находим на странице поле для ввода имени c помощью кастомного типа XPATH
+# Ввод имени пользователя через XPATH
 user_name = driver.find_element(By.XPATH, "//input[@id='user-name']")
-user_name.send_keys('standard_user')  # Ввод имени в поле "usesname"
-time.sleep(1)  # указываем таймер паузы для проверки корректного ввода
+user_name.send_keys('standard_user')
+time.sleep(1)
 
-# С помощью другого типа XPATH (*) означает что тут мы ищем по всей странице Id = 'password'
-
+# Ввод пароля через XPATH (поиск по всей странице через *)
 password = driver.find_element(By.XPATH, "//*[@id='password']")
-password.send_keys('secret_sauce')  # Ввод пароля в поле "password"
-time.sleep(1)  # указываем таймер паузы для проверки корректного ввода
-driver.quit()  # Команда для полного закрытия окна браузера
+password.send_keys('secret_sauce')
+time.sleep(1)
+
+# Завершение теста и закрытие сессии
+driver.quit()
