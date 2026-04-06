1338147be33c88fdf083de1a9c6b972dc1d40cc4
denis <den.podgorod@gmail.com>
2026-04-06 11:58:37 +0300

    style: apply logical spacing and formatting according

--- Dz1/homework_03/hw_03_authorization.py
+++ Dz1/homework_03/hw_03_authorization.py
@@ -5,19 +5,25 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 
 
-options = webdriver.ChromeOptions()
+options: webdriver.ChromeOptions = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
 
-driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
+driver: webdriver.Chrome = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
 
-base_url = 'https://www.saucedemo.com/'
+# Открытие сайта и настройка окна
+base_url: str = 'https://www.saucedemo.com/'
 driver.get(base_url)
 driver.maximize_window()
 
-user_name = driver.find_element(By.ID, 'user-name')  # Находим на странице поле для ввода имени
-user_name.send_keys('standard_user')  # Ввод имени в поле "usesname"
-time.sleep(1)  # указываем таймер паузы для проверки корректного ввода
-password = driver.find_element(By.ID, 'password')  # Находим на странице поле для ввода пароля
-password.send_keys('secret_sauce')  # Ввод пароля в поле "password"
-time.sleep(1)  # указываем таймер паузы для проверки корректного ввода
-driver.quit()  # Команда для полного закрытия окна браузера
+# Ввод логина
+user_name = driver.find_element(By.ID, 'user-name')
+user_name.send_keys('standard_user')
+time.sleep(1)
+
+# Ввод пароля
+password = driver.find_element(By.ID, 'password')
+password.send_keys('secret_sauce')
+time.sleep(1)
+
+# Завершение работы
+driver.quit()
